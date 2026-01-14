064f94e30d8c4e647aba174b8d52196094497b3c
Yaseen1212 <168538962+Yaseen1212@users.noreply.github.com>
2026-01-14 19:01:19 +0300

    Update todo.py

--- todo.py
+++ todo.py
@@ -8,7 +8,7 @@ def show_menu():
     print("2. إضافة مهمة")
     print("3. حذف مهمة")
     print("4. خروج")
-
+  print("5. خروج")
 while True:
     show_menu()
     choice = input("اختر رقم العملية: ")
@@ -30,6 +30,16 @@ while True:
             print("رقم غير صالح.")
     elif choice == '4':
         print("مع السلامة!")
+         if 0 < task_num <= len(tasks):
+            tasks.pop(task_num - 1)
+            print("تم الحذف!")
+        else:
+            print("رقم غير صالح.")
+    elif choice == '5':
+        print("مع السلامة!")
         break
     else:
+
+        
+
         print("اختيار خاطئ، حاول مرة أخرى.")
